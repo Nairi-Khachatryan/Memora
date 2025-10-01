48c9b5a7cd53abd6a9677d40da468aaa5e41688f
Nairi-Khachatryan <nairi.khachatryan357@gmail.com>
2025-10-01 16:43:37 +0400

    uninstall react 19 and use react 18

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -36,7 +36,7 @@ export const Home = () => {
   return (
     <div className={s.homeContainer}>
       <div className={s.homeHeader}>
-        <h1>Hire Will Be Your Family Try</h1>
+        <h1>Family Try</h1>
       </div>
       <div className={s.tryContainer}>
         {syrcleArray.map((item, idx) => (
