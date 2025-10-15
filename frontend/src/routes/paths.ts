ef4b07e63eb728184147f3f61b0ee4964125b680
Nairi-Khachatryan <nairi.khachatryan357@gmail.com>
2025-10-15 23:42:39 +0400

    add .env for api keys

--- frontend/src/routes/paths.ts
+++ frontend/src/routes/paths.ts
@@ -1,4 +1,6 @@
-export const API_AUTH = 'http://localhost:5051/auth';
-export const API_BLOCK = 'http://localhost:5051/block';
-export const API_AVATAR = 'http://localhost:5051/avatar';
-export const API_USER = 'http://localhost:5051/user';
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+export const API_AUTH = `${BASE_URL}/auth`;
+export const API_BLOCK = `${BASE_URL}/block`;
+export const API_AVATAR = `${BASE_URL}/avatar`;
+export const API_USER = `${BASE_URL}/user`;
